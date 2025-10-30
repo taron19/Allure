0bb7889c4abd9021b76924dc6f76c34721caf9a3
Taron Gukasyan <funkadelica@mail.ru>
2025-10-30 15:34:02 +0300

    corrected data

--- src/test/java/allure/pr/AllureSteps.java
+++ src/test/java/allure/pr/AllureSteps.java
@@ -1,7 +1,11 @@
 package allure.pr;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.attachment;
@@ -26,5 +30,15 @@ public class AllureSteps {
     @Step("проверяем что это тип ошибки")
     public void checkBugType(String bug) {
         $$(".prc-Text-Text-0ima0").findBy(Condition.text(bug)).shouldBe(Condition.visible);
+    }
+
+    /**
+     * сериализация нашего скриншота
+     * @param name
+     * @return
+     */
+    @Attachment(value = "{name}", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot(String name) {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
--- src/test/java/allure/pr/TestIssueInGitHub.java
+++ src/test/java/allure/pr/TestIssueInGitHub.java
@@ -6,8 +6,8 @@ import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
-import javax.management.loading.ClassLoaderRepository;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -15,7 +15,7 @@ import static io.qameta.allure.Allure.attachment;
 import static io.qameta.allure.Allure.step;
 
 
-public class Test1 {
+public class TestIssueInGitHub {
 
     private static final String REPOSITORY = "taron19/Allure";
     private static final String BUG_TYPE = "bug";
@@ -28,6 +28,12 @@ public class Test1 {
     }
 
     @Test
+    @Owner("Taron")
+    @DisplayName("Проверка Issue в репозитории github")
+    @Link(value = "GitHub Issue", url = "https://github.com/taron19/Allure/issues")
+    @Severity(SeverityLevel.CRITICAL)           // насколько важен тест
+    @Feature("issue в репозитории")               // крупная функциональность
+    @Story("Создание issue")               // конкретный сценарий/история внутри фичи
     void testSelenide() {
         //для создания детальных и красивых отчетов о тестировании
         SelenideLogger.addListener("allureListener", new AllureSelenide());
@@ -41,14 +47,20 @@ public class Test1 {
     }
 
     @Test
+    @Owner("Taron")
+    @DisplayName("Проверка Issue в репозитории github")
+    @Link(value = "GitHub Issue", url = "https://github.com/taron19/Allure/issues")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("issue в репозитории")
+    @Story("Создание issue")
     void lambdaTestAllure() {
         SelenideLogger.addListener("allureListener", new AllureSelenide());
         step("открываем нужный репозиторий GitHub",
                 () -> open("https://github.com/" + REPOSITORY));
         attachment("source", webdriver().driver().source());
+
         step("кликаем по кнопке Issues ", () ->
                 $("#issues-tab [data-content='Issues']").click());
-        attachment("source", webdriver().driver().source());
         step("проверяем название Issue", () ->
                 $("a[href='/taron19/Allure/issues/1']").shouldHave(Condition.exactText("FirstIssue")));
         step("проверяем что это тип ошибки" + BUG_TYPE,
@@ -57,14 +69,26 @@ public class Test1 {
 
 
     @Test
+    @Owner("Taron")
+    @DisplayName("Проверка Issue в репозитории github")
+    @Link(value = "GitHub Issue", url = "https://github.com/taron19/Allure/issues")
+    @Severity(SeverityLevel.CRITICAL)
+    @Feature("issue в репозитории")
+    @Story("Создание issue")
     void annotationTestAllure() {
         SelenideLogger.addListener("allureListener", new AllureSelenide());
 
         AllureSteps allureSteps = new AllureSteps();
         allureSteps.openPage(REPOSITORY);
+        allureSteps.takeScreenshot("после открытия страницы");
+        //Фото можно также делать
+        //attachment("Screenshot", new ByteArrayInputStream(allureSteps.takeScreenshot("после открытия страницы")));
         allureSteps.clickIssueButton();
         allureSteps.checkNameIssue();
         allureSteps.checkBugType(BUG_TYPE);
+        allureSteps.takeScreenshot("после проверки типа ошибки");
+
+        //в отчете будет 2 скриншота
 
     }
 }
